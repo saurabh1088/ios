84b1d7d00641e51038cf566f35c550b6263d27f1
Saurabh Verma <saurabh1088@gmail.com>
2024-03-08 23:00:33 +0530

    Core Data : Added tableview related logic for MoviesViewController.

--- LearningAppCoreDataUIKit/LearningAppCoreDataUIKit/Movies/MoviesViewController.swift
+++ LearningAppCoreDataUIKit/LearningAppCoreDataUIKit/Movies/MoviesViewController.swift
@@ -14,17 +14,46 @@ class MoviesViewController: UIViewController {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        createRightNavigationBarButton()
+    }
+    
+    
+}
+
+// MARK: -----------------------------------------------------------------------
+// MARK: MoviesViewController Navigation Items
+// TODO: Figure out if instead programmatically this can be added through storyboard itself
+// Current issue is if MoviesStoryboard is again embedded in navigation controller
+// then the show segue starts to get performed as present instead of push.
+extension MoviesViewController {
+    
+    func createRightNavigationBarButton() {
+        let rightBarButtonImage = UIImage(systemName: "plus")
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: rightBarButtonImage,
+                                                                 style: .plain,
+                                                                 target: self,
+                                                                 action: #selector(rightNavigationBarButtonAction))
+    }
+    
+    @objc
+    func rightNavigationBarButtonAction() {
+        print("Right button tapped")
     }
 }
 
+// MARK: -----------------------------------------------------------------------
+// MARK: MoviesViewController UITableViewDelegate conformance
 extension MoviesViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieTableViewCell", for: indexPath)
+        cell.textLabel?.text = "Test"
         return cell
     }
 }
 
+// MARK: -----------------------------------------------------------------------
+// MARK: MoviesViewController UITableViewDelegate conformance
 extension MoviesViewController: UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
