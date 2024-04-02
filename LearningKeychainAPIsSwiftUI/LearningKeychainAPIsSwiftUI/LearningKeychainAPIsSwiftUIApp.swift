853189c82a266f3c2082fc2370788687045aba9c
Saurabh Verma <saurabh1088@gmail.com>
2024-04-02 08:26:59 +0530

    SwiftUI Keychain APIs : Added new project using SwiftUI to explore keychain APIs.

---
+++ LearningKeychainAPIsSwiftUI/LearningKeychainAPIsSwiftUI/LearningKeychainAPIsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningKeychainAPIsSwiftUIApp.swift
+//  LearningKeychainAPIsSwiftUI
+//
+//  Created by Saurabh Verma on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningKeychainAPIsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
