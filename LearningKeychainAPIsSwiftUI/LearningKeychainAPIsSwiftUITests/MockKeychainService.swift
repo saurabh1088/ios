47d55f8eea4861534596025d0376cbf9ab39bed6
Saurabh Verma <saurabh1088@gmail.com>
2024-04-10 08:55:04 +0530

    Added mock keychain service for unit tests.

---
+++ LearningKeychainAPIsSwiftUI/LearningKeychainAPIsSwiftUITests/MockKeychainService.swift
@@ -0,0 +1,30 @@
+//
+//  MockKeychainService.swift
+//  LearningKeychainAPIsSwiftUITests
+//
+//  Created by Saurabh Verma on 10/04/24.
+//
+
+import Foundation
+@testable import LearningKeychainAPIsSwiftUI
+
+class MockKeychainService: KeychainServicesProvider {
+    
+    private var storage = [String: String]()
+    
+    func secure(secret: String, of user: String) {
+        storage[user] = secret
+    }
+    
+    func retrieveSecret(for user: String) -> String? {
+        storage[user]
+    }
+    
+    func update(secret: String, for user: String) {
+        storage[user] = secret
+    }
+    
+    func deleteEntry(for user: String) {
+        storage.removeValue(forKey: user)
+    }
+}
