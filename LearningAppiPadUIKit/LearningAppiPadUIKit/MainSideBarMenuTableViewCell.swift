45c31b16e5eabba748a99da981fb430096c7159b
Saurabh Verma <saurabh1088@gmail.com>
2024-09-09 08:16:30 +0530

    Added custom menu table view cell.

---
+++ LearningAppiPadUIKit/LearningAppiPadUIKit/MainSideBarMenuTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainSideBarMenuTableViewCell.swift
+//  LearningAppiPadUIKit
+//
+//  Created by Saurabh Verma on 09/09/24.
+//
+
+import UIKit
+
+class MainSideBarMenuTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
