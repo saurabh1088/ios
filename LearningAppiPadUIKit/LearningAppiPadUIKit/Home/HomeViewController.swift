7ae36d5ae7961c31afb58c545f5600ce7d808a09
Saurabh Verma <saurabh1088@gmail.com>
2024-09-11 22:40:06 +0530

    Added HomeDetailViewController for secind level navigation from home page.

--- LearningAppiPadUIKit/LearningAppiPadUIKit/Home/HomeViewController.swift
+++ LearningAppiPadUIKit/LearningAppiPadUIKit/Home/HomeViewController.swift
@@ -8,8 +8,16 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var homeLabel: UILabel!
+    @IBOutlet weak var detailsButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func detailsButtonAction(_ sender: Any) {
+        let detailViewController = HomeDetailViewController()
+        self.navigationController?.pushViewController(detailViewController, animated: true)
     }
 }
