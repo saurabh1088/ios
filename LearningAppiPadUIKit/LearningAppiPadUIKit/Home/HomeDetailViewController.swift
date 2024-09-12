021ecf818f430624f9fba27a60cab3bc62038a8a
Saurabh Verma <saurabh1088@gmail.com>
2024-09-12 22:48:03 +0530

    Added back button for HomeDetailViewController and updated the one for HomeViewController.

--- LearningAppiPadUIKit/LearningAppiPadUIKit/Home/HomeDetailViewController.swift
+++ LearningAppiPadUIKit/LearningAppiPadUIKit/Home/HomeDetailViewController.swift
@@ -11,5 +11,19 @@ class HomeDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.leftBarButtonItem = navigationBarBackButton()
+    }
+    
+    @objc func goBack() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    private func navigationBarBackButton() -> UIBarButtonItem {
+        let backBarButton = UIBarButtonItem(image: UIImage(systemName: "arrowshape.backward.fill"),
+                                            style: .plain,
+                                            target: self,
+                                            action: #selector(goBack))
+        backBarButton.tintColor = UIColor(named: "vintageRed")
+        return backBarButton
     }
 }
--- LearningAppiPadUIKit/LearningAppiPadUIKit/Home/HomeViewController.swift
+++ LearningAppiPadUIKit/LearningAppiPadUIKit/Home/HomeViewController.swift
@@ -14,7 +14,7 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.navigationBar.topItem?.leftBarButtonItem = navigationBarBackButton()
+        self.navigationItem.leftBarButtonItem = navigationBarBackButton()
     }
     
     @IBAction func detailsButtonAction(_ sender: Any) {
