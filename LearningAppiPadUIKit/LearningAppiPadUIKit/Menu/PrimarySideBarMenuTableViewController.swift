f0b3349502622bb8198f72c8c6d656f970f2a9d5
Saurabh Verma <saurabh1088@gmail.com>
2024-09-14 10:39:27 +0530

    Added main page for work menu item.

--- LearningAppiPadUIKit/LearningAppiPadUIKit/Menu/PrimarySideBarMenuTableViewController.swift
+++ LearningAppiPadUIKit/LearningAppiPadUIKit/Menu/PrimarySideBarMenuTableViewController.swift
@@ -50,6 +50,15 @@ class PrimarySideBarMenuTableViewController: UITableViewController {
                     let homeViewController = HomeViewController()
                     self.splitViewController?.showDetailViewController(homeViewController, sender: self)
                 }
+            } else if option == .work {
+                if let navigationController = self.splitViewController?.viewControllers.last as? UINavigationController,
+                   let topViewcontroller = navigationController.topViewController,
+                    topViewcontroller is WorkViewController {
+                    print("WorkViewController already on navigation stack, not pushing again")
+                } else {
+                    let workViewController = WorkViewController()
+                    self.splitViewController?.showDetailViewController(workViewController, sender: self)
+                }
             }
         }
     }
---
+++ LearningAppiPadUIKit/LearningAppiPadUIKit/Work/WorkViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WorkViewController.swift
+//  LearningAppiPadUIKit
+//
+//  Created by Saurabh Verma on 14/09/24.
+//
+
+import UIKit
+
+class WorkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
