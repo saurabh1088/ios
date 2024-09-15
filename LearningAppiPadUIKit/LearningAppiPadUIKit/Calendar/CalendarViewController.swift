243e6e17c8d664ac041ceb471a8b75d392e7b45a
Saurabh Verma <saurabh1088@gmail.com>
2024-09-15 10:11:34 +0530

    Added back buttons to work and calendar main page views.

--- LearningAppiPadUIKit/LearningAppiPadUIKit/Calendar/CalendarViewController.swift
+++ LearningAppiPadUIKit/LearningAppiPadUIKit/Calendar/CalendarViewController.swift
@@ -11,5 +11,21 @@ class CalendarViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.leftBarButtonItem = navigationBarBackButton()
+    }
+}
+
+extension CalendarViewController {
+    @objc func goBack() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    private func navigationBarBackButton() -> UIBarButtonItem {
+        let backBarButton = UIBarButtonItem(image: UIImage(systemName: "arrowshape.backward.fill"),
+                                            style: .plain,
+                                            target: self,
+                                            action: #selector(goBack))
+        backBarButton.tintColor = UIColor(named: "vintageRed")
+        return backBarButton
     }
 }
--- LearningAppiPadUIKit/LearningAppiPadUIKit/Work/WorkViewController.swift
+++ LearningAppiPadUIKit/LearningAppiPadUIKit/Work/WorkViewController.swift
@@ -11,5 +11,21 @@ class WorkViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.leftBarButtonItem = navigationBarBackButton()
+    }
+}
+
+extension WorkViewController {
+    @objc func goBack() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    private func navigationBarBackButton() -> UIBarButtonItem {
+        let backBarButton = UIBarButtonItem(image: UIImage(systemName: "arrowshape.backward.fill"),
+                                            style: .plain,
+                                            target: self,
+                                            action: #selector(goBack))
+        backBarButton.tintColor = UIColor(named: "vintageRed")
+        return backBarButton
     }
 }
