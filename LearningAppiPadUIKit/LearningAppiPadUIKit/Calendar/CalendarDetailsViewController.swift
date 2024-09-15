b3c63d54d084cfa43bcd70b02bd3daae079afa1f
Saurabh Verma <saurabh1088@gmail.com>
2024-09-15 10:30:01 +0530

    Added calendar details view.

--- LearningAppiPadUIKit/LearningAppiPadUIKit/Calendar/CalendarDetailsViewController.swift
+++ LearningAppiPadUIKit/LearningAppiPadUIKit/Calendar/CalendarDetailsViewController.swift
@@ -1,32 +1,21 @@
 //
-//  CalendarViewController.swift
+//  CalendarDetailsViewController.swift
 //  LearningAppiPadUIKit
 //
-//  Created by Saurabh Verma on 14/09/24.
+//  Created by Saurabh Verma on 15/09/24.
 //
 
 import UIKit
 
-class CalendarViewController: UIViewController {
+class CalendarDetailsViewController: UIViewController {
 
-    
-    @IBOutlet weak var calendarLabel: UILabel!
-    
-    @IBOutlet weak var detailsButton: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.leftBarButtonItem = navigationBarBackButton()
     }
-    
-    @IBAction func detailsButtonAction(_ sender: Any) {
-        let calendarDetailsViewController = CalendarDetailsViewController()
-        self.navigationController?.pushViewController(calendarDetailsViewController, animated: true)
-    }
-    
 }
 
-extension CalendarViewController {
+extension CalendarDetailsViewController {
     @objc func goBack() {
         self.navigationController?.popViewController(animated: true)
     }
