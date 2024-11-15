71bb68a716b900bfc3dacb4b6b5130f7d5b0b682
Saurabh Verma <saurabh1088@gmail.com>
2024-11-15 06:50:58 +0530

    Added collection view for laying out calndar view.

---
+++ LearningAppiPadUIKit/LearningAppiPadUIKit/Calendar/CalendarCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarCollectionViewCell.swift
+//  LearningAppiPadUIKit
+//
+//  Created by Saurabh Verma on 29/09/24.
+//
+
+import UIKit
+
+class CalendarCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dayNumberLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
--- LearningAppiPadUIKit/LearningAppiPadUIKit/Calendar/CalendarDetailsViewController.swift
+++ LearningAppiPadUIKit/LearningAppiPadUIKit/Calendar/CalendarDetailsViewController.swift
@@ -18,6 +18,16 @@ class CalendarDetailsViewController: UIViewController,
         self.navigationItem.leftBarButtonItem = navigationBarBackButton()
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
+        /// Learning
+        ///
+        /// Here using `register(_ nib: UINib?, forCellWithReuseIdentifier identifier: String)` works.
+        ///
+        /// However if `register(_ cellClass: AnyClass?, forCellWithReuseIdentifier identifier: String)` is
+        /// used, it results in crash with custom cells IBOutlet properties being nil. So below code doesn't works here
+        /// ```
+        /// self.collectionView.register(CalendarCollectionViewCell.self, forCellWithReuseIdentifier: "calendarCollectionViewCell")
+        /// ```
+        self.collectionView.register(UINib(nibName: "CalendarCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "calendarCollectionViewCell")
     }
 }
 
@@ -38,10 +48,14 @@ extension CalendarDetailsViewController {
 
 extension CalendarDetailsViewController {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 7
+        return 30
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "calendarCollectionViewCell", for: indexPath) as? CalendarCollectionViewCell {
+            cell.dayNumberLabel.text = String(indexPath.row + 1)
+            return cell
+        }
         return UICollectionViewCell()
     }
 }
