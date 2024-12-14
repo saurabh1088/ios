5b4bd195e0eb7479419df30a81b46f44512ed2c7
Saurabh Verma <saurabh1088@gmail.com>
2024-12-14 10:08:55 +0530

    Added example for UIView frame changing position of view.

--- LearningAppFrameBoundCenterUIKit/LearningAppFrameBoundCenterUIKit/FrameViewController.swift
+++ LearningAppFrameBoundCenterUIKit/LearningAppFrameBoundCenterUIKit/FrameViewController.swift
@@ -14,6 +14,8 @@ class FrameViewController: UIViewController {
     @IBOutlet weak var exampleSubViewOne: UIView!
     @IBOutlet weak var animateFrameButton: UIButton!
     
+    @IBOutlet weak var animateFramePositionButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Current frame of exampleSubViewOne \(exampleSubViewOne.frame)")
@@ -25,6 +27,16 @@ class FrameViewController: UIViewController {
             let originalX = self.exampleSubViewOne.frame.origin.x
             let originalY = self.exampleSubViewOne.frame.origin.y
             exampleSubViewOne.frame = CGRect(x: originalX, y: originalY, width: 200, height: 100)
+        }
+    }
+    
+    
+    @IBAction func animateFramePositionAction(_ sender: Any) {
+        UIView.animate(withDuration: 1) { [weak self] in
+            guard let self else { return }
+            let originalWidth = self.exampleSubViewOne.frame.width
+            let originalHeight = self.exampleSubViewOne.frame.height
+            exampleSubViewOne.frame = CGRect(x: 0, y: 0, width: originalWidth, height: originalHeight)
         }
     }
     
