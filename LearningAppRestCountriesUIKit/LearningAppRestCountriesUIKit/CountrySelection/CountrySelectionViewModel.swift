c6621e50559db82ec353abbe3790687977cab0d6
Saurabh Verma <saurabh1088@gmail.com>
2025-01-25 12:01:16 +0530

    Added skeleton CountrySelectionViewModel for CountrySelectionViewController.

---
+++ LearningAppRestCountriesUIKit/LearningAppRestCountriesUIKit/CountrySelection/CountrySelectionViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  CountrySelectionViewModel.swift
+//  LearningAppRestCountriesUIKit
+//
+//  Created by Saurabh Verma on 25/01/25.
+//
+
+import Foundation
+
+protocol CountrySelectionViewModelProtocol {}
+
+class CountrySelectionViewModel: CountrySelectionViewModelProtocol {
+    
+}
