8b7dd370f36c7bb7e08452f02fbb68e52f43582a
Saurabh Verma <saurabh1088@gmail.com>
2025-01-28 11:18:37 +0530

    Added option to check response on a textview, as a temporary change to validate response.

--- LearningAppRestCountriesUIKit/LearningAppRestCountriesUIKit/CountryDetails/CountryDetailsViewController.swift
+++ LearningAppRestCountriesUIKit/LearningAppRestCountriesUIKit/CountryDetails/CountryDetailsViewController.swift
@@ -8,13 +8,24 @@
 import UIKit
 
 class CountryDetailsViewController: UIViewController {
-
+    
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         Task {
             do {
                 let response = try await CountryDetailsService().fetchDetailsFor(country: "japan", with: FetchCountryDetailsRequest(countryName: "japan"))
-                print(response)
+                
+                let jsonData = try JSONEncoder().encode(response)
+                let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: [])
+                let prettyPrintedData = try JSONSerialization.data(withJSONObject: jsonObject, options: [.prettyPrinted])
+                
+                if let prettyPrintedString = String(data: prettyPrintedData, encoding: .utf8) {
+                    textView.text = prettyPrintedString
+                } else {
+                    textView.text = "Could not convert to string"
+                }
             } catch(let error) {
                 print("Error \(error)")
             }
