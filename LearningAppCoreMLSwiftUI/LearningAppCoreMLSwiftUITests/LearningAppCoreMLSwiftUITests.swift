8af0ea8b33816a4474d6ad7f795e76132e91e6ff
Saurabh Verma <saurabh1088@gmail.com>
2025-02-27 18:55:16 +0530

    Starter project LearningAppCoreMLSwiftUI.

---
+++ LearningAppCoreMLSwiftUI/LearningAppCoreMLSwiftUITests/LearningAppCoreMLSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppCoreMLSwiftUITests.swift
+//  LearningAppCoreMLSwiftUITests
+//
+//  Created by Saurabh Verma on 27/02/25.
+//
+
+import Testing
+@testable import LearningAppCoreMLSwiftUI
+
+struct LearningAppCoreMLSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
