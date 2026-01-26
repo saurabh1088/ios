c54ba0754a3e5820720834c0cd232a228c556faf
Saurabh Verma <saurabh1088@gmail.com>
2026-01-26 17:36:29 +0530

    Added new project LearningKeychainAPIsSwiftUI.

---
+++ LearningAppSQLiteSwiftUI/LearningAppSQLiteSwiftUI/LearningAppSQLiteSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppSQLiteSwiftUIApp.swift
+//  LearningAppSQLiteSwiftUI
+//
+//  Created by Saurabh Verma on 26/01/26.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppSQLiteSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearningAppSQLiteSwiftUI/LearningAppSQLiteSwiftUITests/LearningAppSQLiteSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppSQLiteSwiftUITests.swift
+//  LearningAppSQLiteSwiftUITests
+//
+//  Created by Saurabh Verma on 26/01/26.
+//
+
+import Testing
+@testable import LearningAppSQLiteSwiftUI
+
+struct LearningAppSQLiteSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
